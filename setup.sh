cc4e9344f0790e926b0bd7f172f4a695fb3bbb50
Andrew Jones <andrew@andrewjones.id.au>
2014-11-27 23:14:34 +1100

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,31 +1,35 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -e
+
+BASE_PATH=/opt
+PIXELATED_PATH=$BASE_PATH/pixelated-user-agent
+mkdir -p $BASE_PATH
 
 sudo apt-get install -y nodejs npm ruby bundler git python-dev libffi-dev libevent-dev nodejs-legacy phantomjs
 
-git clone https://github.com/pixelated-project/pixelated-user-agent /vagrant/pixelated-user-agent/
+git clone https://github.com/pixelated-project/pixelated-user-agent $PIXELATED_PATH/
 
-cd /vagrant
+cd $BASE_PATH
 curl -O https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.9.tar.gz
 tar xvfz virtualenv-1.9.tar.gz
 
-cd /vagrant/virtualenv-1.9
+cd $BASE_PATH/virtualenv-1.9
 sudo python setup.py install
 
-cd /vagrant/pixelated-user-agent/
+cd $PIXELATED_PATH/
 ./install-pixelated.sh
 
-cd /vagrant/pixelated-user-agent/service
+cd $PIXELATED_PATH/service
 virtualenv .virtualenv
 source .virtualenv/bin/activate
 python setup.py develop --always-unzip
 
-cd /vagrant/pixelated-user-agent/web-ui
+cd $PIXELATED_PATH/web-ui
 npm install
 sudo npm install -g bower
 bower install
 sudo npm install -g grunt-cli
 ./go build
 
-cd /vagrant/pixelated-user-agent/service
+cd $PIXELATED_PATH/service
 source .virtualenv/bin/activate
